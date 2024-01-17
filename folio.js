b13320eaad6cc5517a77e0dc2de52e5d7a1ecda9
kashish-shukla29 <kashishshuklachp@gmail.com>
2024-01-17 12:13:36 +0530

    my portfolio profile

---
+++ folio.js
@@ -0,0 +1,12 @@
+// var typed = new Typed(".typing",{
+//     Strings:["",",Full Stack Developer","Front End Developer","Back End Developer"],
+//     typSpeed:100,
+//     backSpeed:60,
+//     loop:true,
+// })
+var typed = new Typed('.typing', {
+    strings: ["","Full Stack Developer","","Meren StackDeveloper","Front End Developer","HTML,CSS,Javascript,React.JS ","Back End Developer","MongoDB,NodeJS & ExpressJS"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true,
+  });
